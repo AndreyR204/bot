382b7be3a04f6a8b11d9e2f279cb994a68e4fc9b
Andrey <andrejrybenok@gmail.com>
2022-06-09 21:23:04 +0500

    changes

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -2,20 +2,16 @@ package bot;
 
 import logic.FoodRecorder;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.api.objects.Message;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-//#TODO:2. Отделяемость логики от телеграма
 //#TODO:5. Убрать Config.txt, добавить все кодом
 
 public class Bot extends TelegramLongPollingBot {
